abdec8e332e88a3ae0ada58cf0bb45853c7ec9d2
Rafal Sladek <rafal.sladek@gmail.com>
2016-08-09 23:53:21 +0200

    added iron router and bootstrap as well a first draft of the navi

--- client/main.js
+++ client/main.js
@@ -1,22 +1,22 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+//
+// import './main.html';
 
-import './main.html';
+// set up the main template the the router will use to build pages
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+// specify the top level route, the page users see when they arrive at the site
+Router.route('/', function () {
+  console.log("rendering root /");
+  this.render("navbar", {to: "header"});
+  this.render("startScreen", {to: "main"});
+});
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
